4654acff7d673d622c7eafdee4106ac3673f68e3
apis8 <apis80@mail.ru>
2022-07-06 23:16:48 +0100

    комит

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,65 @@ public class Main {
     public static void main(String[] args) {
         // Task 1
         int year = 2022;
-    }
-        private static boolean isLeapYear (int year) {
-          return  year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
-        }
-        private static void printlnLeapYear (int year) {
-            System.out.println("Год високосный");
-            System.out.println("Год високосный");
-        }
-        private static void printlnNonLeapYear (int year) {
-        System.out.println("Год не високосный");
-    }
-    public static void printYearInfo (int year) {
-    if (isLeapYear (year)) {
-        printlnLeapYear(year);
-            } else {
-        printlnNonLeapYear(year);
+        printLeapYear(year);
 
-        }
-        //Task2
-    }
+
+
+    // Task2
+    suggestInstallOs(0, 2019);
+    // Task 3
+        calculateDeliveryDays(150);
+
+
 }
+
+
+    private static boolean isLeapYear(int year) {
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    }
+
+    private static void printLeapYear(int year) {
+        if (isLeapYear(year)) {
+            System.out.println("Год високосный");
+        } else {
+            System.out.println("Год не високосный");
+        }
+    }
+
+    private static void suggestInstallOs(int os, int deviceYear) {
+        String resultString = buildResultString(os, deviceYear);
+        System.out.println(resultString);
+
+    }
+    private static int calculateDeliveryDays(int distance){
+        int time = distance/40 +1;
+        System.out.println( "дней доставки: " + time );
+        return distance;
+
+    }
+
+
+    private static String buildResultString(int os, int deviceYear) {
+        int currentYear = 2015;
+        StringBuilder stringBuilder = new StringBuilder();
+        if (deviceYear < currentYear) {
+            stringBuilder.append(" Установите лайт версию для ");
+        } else {
+            stringBuilder.append(" Установите полную версию для ");
+        }
+        if (os == 1) {
+            stringBuilder.append(" Android ");
+        } else {
+            stringBuilder.append("IOS");
+        }
+        return stringBuilder.toString();
+
+    }
+
+}
+
+
+
+
+
+
